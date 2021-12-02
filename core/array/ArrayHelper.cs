6a80fda918b569674810aa088e3136923caaff15
plochish <megahoneybadger@gmail.com>
2021-12-03 00:40:34 +0900

    Fix [FindMissingNumber] algorithm.

--- core/array/ArrayHelper.cs
+++ core/array/ArrayHelper.cs
@@ -1,6 +1,7 @@
 ﻿#region Usings
 using System;
 using System.Collections.Generic;
+using System.Linq;
 #endregion
 
 namespace Abbyy.Vantage.Utils
@@ -17,80 +18,118 @@ namespace Abbyy.Vantage.Utils
 		/// </summary>
 		/// <param name="nums">Input array of length n - 1.</param>
 		/// <returns>Two missing numbers as an array</returns>
-		public static int[] FindMissingNumbers( int [] nums ) 
+		public static int[] FindMissingNumbers( int [] arr ) 
 		{
-			var len = nums.Length;
+			var n = arr.Length + 1;
 
-			var min = 0;
-			var max = len + 1; 
+			// Sum of 2 missing numbers
+			int sum = ( n * ( n + 1 ) ) / 2 -	arr.Sum();
 
-			var hasMin = false;
-			var hasMax = false;
+			// Find average of two elements
+			int avg = ( sum / 2 );
 
-			for( int i = 0; i < len; ++i )
+			int sumSmallerHalf = 0,	sumGreaterHalf = 0;
+
+			for( int i = 0; i < n - 1; i++ )
 			{
-				var n = nums [ i ];
-
-				if( n <= 0 || n >= max )
+				if( arr [ i ] <= avg )
 				{
-					nums [ i ] = max + 1;
+					sumSmallerHalf += arr [ i ];
 				}
-
-				if( n == min ) 
+				else
 				{
-					hasMin = true;
-				}
-
-				if( n == max ) 
-				{
-					hasMax = true;
+					sumGreaterHalf += arr [ i ];
 				}
 			}
 
-			if( !hasMin && !hasMax )
-				return new [] { min, max };
+			int totalSmallerHalf = ( avg * ( avg + 1 ) ) / 2;
 
-			// let's use values as pigeon holes (i.e. as indexes)
+			var first = totalSmallerHalf - sumSmallerHalf;
 
-			for( int i = 0; i < len; ++i )
-			{
-				var n = nums [ i ];
-				var index = Math.Abs( n );
+			var second = ( ( n * ( n + 1 ) ) / 2 - totalSmallerHalf ) - sumGreaterHalf;
 
-				if( index > max )
-					continue;
+			return new [] { first, second };
+		}
+		/// <summary>
+		/// Find two missing numbers in an array  [0..n]
+		/// </summary>
+		/// <param name="nums">Input array of length n - 1.</param>
+		/// <returns>Two missing numbers as an array</returns>
+		public static int [] FindMissingNumbersOld( int [] nums )
+		{
+			//var len = nums.Length;
 
-				--index;
+			//var min = 0;
+			//var max = len + 1;
 
-				if( nums [ index ] > 0 )
-				{
-					nums [ index ] *= -1;
-				}
-			}
+			//var hasMin = false;
+			//var hasMax = false;
 
-			var list = new List<int>();
+			//for( int i = 0; i < len; ++i )
+			//{
+			//	var n = nums [ i ];
 
-			for( int i = 0; i < len; ++i )
-			{
-				var n = nums [ i ];
+			//	if( n <= 0 || n >= max )
+			//	{
+			//		nums [ i ] = max + 1;
+			//	}
 
-				if( n >= 0 ) 
-				{
-					list.Add( i + 1 );
-				}
-			}
+			//	if( n == min )
+			//	{
+			//		hasMin = true;
+			//	}
 
-			if( !hasMin ) 
-			{
-				list.Add( min );
-			}
+			//	if( n == max )
+			//	{
+			//		hasMax = true;
+			//	}
+			//}
 
-			if( !hasMax )
-			{
-				list.Add( max );
-			}
+			//if( !hasMin && !hasMax )
+			//	return new [] { min, max };
 
-			return list.ToArray();
+			//// let's use values as pigeon holes (i.e. as indexes)
+
+			//for( int i = 0; i < len; ++i )
+			//{
+			//	var n = nums [ i ];
+			//	var index = Math.Abs( n );
+
+			//	if( index > max )
+			//		continue;
+
+			//	--index;
+
+			//	if( nums [ index ] > 0 )
+			//	{
+			//		nums [ index ] *= -1;
+			//	}
+			//}
+
+			//var list = new List<int>();
+
+			//for( int i = 0; i < len; ++i )
+			//{
+			//	var n = nums [ i ];
+
+			//	if( n >= 0 )
+			//	{
+			//		list.Add( i + 1 );
+			//	}
+			//}
+
+			//if( !hasMin )
+			//{
+			//	list.Add( min );
+			//}
+
+			//if( !hasMax )
+			//{
+			//	list.Add( max );
+			//}
+
+			//return list.ToArray();
+			return null;
 		}
 		#endregion
 	}
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,5 +1,7 @@
 ﻿#region Usings
+using Abbyy.Vantage.Utils;
 using Abbyy.Vantage.Utils.Sandbox;
 #endregion
 
 new Repl().Run();
+
